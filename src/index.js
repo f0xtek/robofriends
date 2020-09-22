59bf0ce1b05e916fa2a21120afa93df3ae1a6283
Luke Anderson <landerson61@gmail.com>
2020-09-22 05:33:41 +0100

    Initial commit
    Added Robot card components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CardList from './CardList';
+import { robots } from './robots';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CardList robots={robots}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/robots.js
@@ -0,0 +1,46 @@
+export const robots = [
+  {
+    "id": 1,
+    "name": "Karen Ninnoli",
+    "email": "kninnoli0@usnews.com"
+  },
+  {
+    "id": 2,
+    "name": "Bobbi Van Salzberger",
+    "email": "bvan1@typepad.com"
+  },
+  {
+    "id": 3,
+    "name": "Jilleen Poppleston",
+    "email": "jpoppleston2@newsvine.com"
+  },
+  {
+    "id": 4,
+    "name": "Ronda Hansley",
+    "email": "rhansley3@illinois.edu"
+  },
+  {
+    "id": 5,
+    "name": "Sven St Clair",
+    "email": "sst4@furl.net"
+  },
+  {
+    "id": 6,
+    "name": "Colan Bonicelli",
+    "email": "cbonicelli5@jugem.jp"
+  },
+  {
+    "id": 7,
+    "name": "Eva McSperron",
+    "email": "emcsperron6@google.com.br"
+  },
+  {
+    "id": 8,
+    "name": "Guthrie Biesinger",
+    "email": "gbiesinger7@prnewswire.com"
+  }, {
+    "id": 9,
+    "name": "Brett Spadeck",
+    "email": "bspadeck8@ning.com"
+  }
+]
