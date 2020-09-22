ab5c3493971860af6025dbdea4fa8f1a3f58c937
Luke Anderson <landerson61@gmail.com>
2020-09-22 07:29:04 +0100

    Add some styling to the App

--- src/robots.js
+++ src/robots.js
@@ -42,5 +42,45 @@ export const robots = [
     "id": 9,
     "name": "Brett Spadeck",
     "email": "bspadeck8@ning.com"
+  }, {
+    "id": 10,
+    "name": "Tomas Dorsett",
+    "email": "tdorsett0@yahoo.com"
+  }, {
+    "id": 11,
+    "name": "Willi Meltetal",
+    "email": "wmeltetal1@reddit.com"
+  }, {
+    "id": 12,
+    "name": "Amalee Capsey",
+    "email": "acapsey2@usatoday.com"
+  }, {
+    "id": 13,
+    "name": "Dot Jarnell",
+    "email": "djarnell3@gnu.org"
+  }, {
+    "id": 14,
+    "name": "Trip Finnan",
+    "email": "tfinnan4@hao123.com"
+  }, {
+    "id": 15,
+    "name": "Rhianon Brotherhead",
+    "email": "rbrotherhead5@msn.com"
+  }, {
+    "id": 16,
+    "name": "Jamima Chaudrelle",
+    "email": "jchaudrelle6@japanpost.jp"
+  }, {
+    "id": 17,
+    "name": "Rosmunda Elacoate",
+    "email": "relacoate7@redcross.org"
+  }, {
+    "id": 18,
+    "name": "Di Bartrop",
+    "email": "dbartrop8@prweb.com"
+  }, {
+    "id": 19,
+    "name": "Adelle Poller",
+    "email": "apoller9@vkontakte.ru"
   }
 ]
