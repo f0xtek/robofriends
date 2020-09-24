88b0a2b1523828ee2c00ae641cf30c9dd47710a1
Luke Anderson <landerson61@gmail.com>
2020-09-24 06:42:12 +0100

    Refactor CardList component to use `user` item
    Using robots[i] is unnecessary in map as we can access the properties from each `user`.

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -5,13 +5,13 @@ const CardList = ({robots}) => {
     return (
         <div>
             {
-                robots.map((user, i) => {
+                robots.map(user => {
                     return (
                         <Card
-                            key={robots[i].id}
-                            id={robots[i].id}
-                            name={robots[i].name}
-                            email={robots[i].email}
+                            key={user.id}
+                            id={user.id}
+                            name={user.name}
+                            email={user.email}
                         />
                     )
                 })
